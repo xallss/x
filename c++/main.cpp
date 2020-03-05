3fa82a5e2d5df9935803996390cac6991c9a126b
xallss <zhaoym1119@gmail.com>
2020-03-05 23:45:36 +0800

    add Sales_item.h

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,4 +1,28 @@
 #include<iostream>
-int main() {
-    std::cout << "xallss" << std::endl;
+#include"Sales_item.h"
+using namespace std;
+int main()
+{
+	Sales_item total;
+	if (cin >> total)
+	{
+		Sales_item trans;
+		while (cin >> trans)
+		{
+			if (total.isbn == trans.isbn)
+				total += trans;
+			else
+			{
+				cout << total << endl;
+				total = trans;
+			}
+		}
+		cout << total << endl;
+	}
+	else
+	{
+		cerr << "No data?!" << endl;
+		return-1;
+	}
+	system("pause");
 }
