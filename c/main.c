e3384987a8d9b3cd3e1698b1213152ace1d4a21e
xallss <zhaoym1119@gmail.com>
2020-03-07 00:04:22 +0800

    nothing

--- c/main.c
+++ c/main.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 int main() {
-    printf("xallss\n");
+    printf("          xallss       \n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+char *s = NULL;
+test_func(s);
+    return 0;
+}
+
+int test_func(char *s) {
+    if (s == NULL) {
+        fprintf(stderr, "%s: received null pointer argument\n", __func__);
+        return -1;
+    }
     return 0;
 }
